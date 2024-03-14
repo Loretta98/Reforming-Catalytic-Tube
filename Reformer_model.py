dad8a42a0c12c8c78f1be2dce51200ddb9a07168
Loretta Salano <etta.salano@gmail.com>
2024-03-14 17:53:36 +0100

    Model Corrected
    Modello Keq Nielsen - Corrected Xu-Froment

--- Reformer_model.py
+++ Reformer_model.py
@@ -120,21 +120,30 @@ def TubularReactor(z,y,Epsilon,Dp,m_gas,Aint,MW,nu,R,dTube,Twin,RhoC):
 ##################################################################
 # Kinetic Costant and Rate of reaction (Xu-Froment)
     # CH4 + H2O -> CO + 3 H2 ; CO + H2O -> CO2 + H2 ; CH4 + 2H2O +> CO2 + 4H2
-    
-    # Equilibrium constants Twigg coverted to [bar]
-    alpha = 1000/T - 1
-    Keq1 = 1.01325 / (np.exp(alpha*(alpha*(alpha*(0.2513*alpha-0.3665)-0.58101)+27.1337)-3.277))
-    Keq2 = np.exp(alpha*(alpha*(0.63508-0.29353*alpha)+4.1778)+0.31688)
+
+    # Equilibrium constants fitted from Nielsen in [bar]
+    gamma = np.array([[-757.323e5, 997.315e3, -28.893e3, 31.29], [-646.231e5, 563.463e3, 3305.75,-3.466]])
+    Keq1 = np.exp(gamma[0,0]/(T**3)+gamma[0,1]/(T**2)+gamma[0,2]/T + gamma[0,3])
+    Keq2 = np.exp(gamma[1,0]/(T**3)+gamma[1,1]/(T**2)+gamma[1,2]/T + gamma[1,3])
     Keq3 = Keq1*Keq2
+       
     # Arrhenius     I,       II,        III
+    Tr_a = 648                                                                          # K 
+    Tr_b = 823                                                                          # K 
     k0 = np.array([1.842e-4, 7.558,     2.193e-5])                                      # pre exponential factor @648 K 
     E_a = np.array([240.1,   67.13,     243.9])                                         # activation energy [kJ/mol]
-    kr = k0*np.exp(E_a/R/T)
-    # Van't Hoff    CO,     H2,         CH4,    H2O
-    K0 = np.array([40.91,   0.0296,     0.1791, 0.4152])                                # pre exponential factor @648,823 K
-    DH0 = np.array([-70.65, -82.90,     -38.28,  88.68])                                # adsorption enthalpy [kJ/mol]
-    Kr = K0*np.exp(DH0*1000/R/T)
+    kr = k0*np.exp(-E_a/R*(1/T-1/Tr_a))
 
+    # Van't Hoff    CO,     H2,         CH4,    H2O
+    K0_a = np.array([40.91,   0.0296])                                     # pre exponential factor @648 K
+    DH0_a = np.array([-70.65, -82.90])                                     # adsorption enthalpy [kJ/mol]
+    K0_b = np.array([0.1791, 0.4152])                                # pre exponential factor @823 K
+    DH0_b = np.array([-38.28,  88.68])                                # adsorption enthalpy [kJ/mol]
+    
+    Kr_a = K0_a*np.exp(-DH0_a*1000/R*(1/T-1/Tr_a))
+    Kr_b = K0_b*np.exp(-DH0_b*1000/R*(1/T-1/Tr_b))
+    
+    Kr = np.concatenate((Kr_a,Kr_b))
     # Components  [CH4, CO, CO2, H2, H2O, O2, N2]
     DEN = 1 + Kr[0]*Pi[1] + Kr[1]*Pi[3] + Kr[2]*Pi[0] + Kr[3]*Pi[4]/Pi[3]
     rj = np.array([ (kr[0]/Pi[3]**(5/2)) * (Pi[0]*Pi[4]-(Pi[3]**3)*Pi[1]/Keq1) / DEN**2 , (kr[1]/Pi[3]) * (Pi[1]*Pi[4]-Pi[3]*Pi[2]/Keq2) / DEN**2 , (kr[2]/Pi[3]**(7/2)) * (Pi[0]*(Pi[4]**2)-(Pi[3]**4)*Pi[2]/Keq3) / DEN**2 ]) * RhoC * (1-Epsilon) *  1000 #
